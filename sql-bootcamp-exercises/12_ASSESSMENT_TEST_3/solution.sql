79eef1880611dd52589ecd261343219a2196ef14
defendimattia <defendi.mattia92@gmail.com>
2025-11-05 12:05:15 +0100

    Add solution with teachers table

--- sql-bootcamp-exercises/12_ASSESSMENT_TEST_3/solution.sql
+++ sql-bootcamp-exercises/12_ASSESSMENT_TEST_3/solution.sql
@@ -7,3 +7,14 @@ CREATE TABLE
         email VARCHAR(100) NOT NULL UNIQUE CHECK(email ~* '^[A-Z0-9._-]+@[A-Z0-9.-]+\.[A-Z]{2,10}$'),
         graduation_year SMALLINT NOT NULL CHECK(graduation_year > 0)
     );
+
+CREATE TABLE 
+    teachers (
+        teacher_id SERIAL PRIMARY KEY,
+        first_name VARCHAR(50) NOT NULL CHECK(first_name ~* '^[a-z]+$'),
+        last_name VARCHAR(50) NOT NULL CHECK(last_name ~* '^[a-z]+$'),
+        homeroom_number SMALLINT NOT NULL CHECK (homeroom_number > 0),
+        department VARCHAR(50) NOT NULL,
+        phone VARCHAR(20) NOT NULL UNIQUE CHECK(phone ~ '^[+]?[0-9]{6,20}$'),
+        email VARCHAR(100) NOT NULL UNIQUE CHECK(email ~* '^[A-Z0-9._-]+@[A-Z0-9.-]+\.[A-Z]{2,10}$')
+    );
